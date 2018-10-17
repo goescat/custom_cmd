a8a26e89abeb7ce56fe5544e29d02d923d996a29
goescat <goescat1024@gmail.com>
2018-10-17 15:55:54 +0800

    Create cmd_ailas.bat

---
+++ cmd_ailas.bat
@@ -0,0 +1,13 @@
+doskey cat     = type $*
+doskey clear   = cls
+doskey cp      = copy $*
+doskey kill    = taskkill /PID $*
+doskey ls      = dir $*
+doskey man     = help $*
+doskey mv      = move $*
+doskey ps      = tasklist $*
+doskey pwd     = cd
+doskey rm      = del $*
+doskey rmr     = deltree $*
+doskey cd~ = cd C:\Users\user_name
+cls
